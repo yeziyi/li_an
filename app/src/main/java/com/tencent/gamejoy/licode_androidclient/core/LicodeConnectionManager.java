00933f1cecd28043196f1eee3fdbdd1e56fb4009
yijiajia <yijiajia1988@gmail.com>
2018-12-05 21:32:03 +0800

    commit

---
+++ app/src/main/java/com/tencent/gamejoy/licode_androidclient/core/LicodeConnectionManager.java
@@ -0,0 +1,15 @@
+package com.tencent.gamejoy.licode_androidclient.core;
+
+/**
+ * 像licode服务器发起业务请求
+ * 接收服务器的消息推送
+ * 向licode服务器推流(publish)，拉取licode服务器的音视频流(subscribe)
+ * 推拉流通过webrtc peerconnection完成
+ * <p>
+ * https://github.com/lynckia/licode
+ * https://webrtc.org/native-code/android/
+ *
+ * @author watsonxie
+ */
+public class LicodeConnectionManager implements ConnectionInterface {
+}
