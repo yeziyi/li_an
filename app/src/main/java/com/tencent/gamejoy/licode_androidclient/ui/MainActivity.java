c1987909c462f7838666752cda4df3544cdf28f1
yijiajia <yijiajia1988@gmail.com>
2018-12-05 21:05:19 +0800

    commit

---
+++ app/src/main/java/com/tencent/gamejoy/licode_androidclient/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tencent.gamejoy.licode_androidclient.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tencent.gamejoy.licode_androidclient.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
