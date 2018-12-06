d54cbedc024128ab7e0a3ed8ab44c89d89de0f1e
yijiajia <yijiajia1988@gmail.com>
2018-12-06 13:36:27 +0800

    commit

---
+++ app/src/main/java/com/tencent/gamejoy/licode_androidclient/core/ECRoom.java
@@ -0,0 +1,10 @@
+package com.tencent.gamejoy.licode_androidclient.core;
+
+/**
+ * It represents a Licode Room. It will handle the connection, local stream publication and remote stream subscription.
+ * https://licode.readthedocs.io/en/master/client_api/#room
+ *
+ * @author watsonxie
+ */
+public class ECRoom implements ECRoomInterface{
+}
--- app/src/main/java/com/tencent/gamejoy/licode_androidclient/core/ECStream.java
+++ app/src/main/java/com/tencent/gamejoy/licode_androidclient/core/ECStream.java
@@ -1,4 +1,10 @@
 package com.tencent.gamejoy.licode_androidclient.core;
 
+/**
+ * You will use this API to handle connections to rooms and streams in your web applications.
+ * https://licode.readthedocs.io/en/master/client_api/#stream
+ *
+ * @author watsonxie
+ */
 public class ECStream implements ECStreamInterface {
 }
